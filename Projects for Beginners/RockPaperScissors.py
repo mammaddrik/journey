131aca8eea448c7c10cd8f7dbdf57981d6619549
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2024-02-26 23:17:24 +0330

    Create RockPaperScissors.py

---
+++ Projects for Beginners/RockPaperScissors.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#Rock Paper Scissors
+#Github: mammaddrik
+
+#::::: Library :::::
+import random
+import os
+
+def clearScr():
+    "A Function To Clean Up The Command Prompt or Terminal."
+    if (os.name == "nt"):
+        os.system("cls")
+    else:
+        os.system("clear")
+
+while True:
+    clearScr()
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
